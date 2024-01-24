45921f604616e2716be6401344d54525c75d325a
Goru <abhiabhi9087@gmail.com>
2024-01-24 12:50:17 +0530

    cleanup

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,11 @@
 import '@/styles/globals.css'
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
+
 
 const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
 
+    <Component {...pageProps} />
   )
 }
